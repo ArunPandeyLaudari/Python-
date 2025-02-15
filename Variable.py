09914fbd8c54341ab38037c54e7fa2275b125602
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-02-15 19:44:48 +0400

    first commit

---
+++ Variable.py
@@ -0,0 +1,16 @@
+
+# int,float,string,bool varible eg:
+
+name="Arun"
+age=12
+salary=234.45
+is_student=True
+
+print(f"Name is : {name} and age is : {age} and Salaray is {salary} ")
+
+if is_student:
+    print("You are student")
+else:
+    print("You are not student")
+
+print(type(age))
