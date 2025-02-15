fcea04baa9864beaa267eb430af59056b99e3556
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-02-15 20:02:41 +0400

    Input function fetching value in str,int,float from user:

---
+++ input.py
@@ -0,0 +1,10 @@
+# input function is used to fetch the data input from the user default it is used string value if we want to fetch the value in int(),float()... we can use type conversion
+
+name=input("Enter Your naame:")
+print(f"Your Name is :{name}")
+
+age=int(input("Enter Your age :"))
+print(f"Your age is : {age}")
+
+Salary=float(input("Enter Your salary:"))
+print(f"Your Salary is : {Salary}")
---
+++ typeconversion.py
@@ -0,0 +1,13 @@
+# converting one typr of varible in to another
+
+# int(),float(),str(),bool()
+
+age=12
+sal=234.0
+boool=True
+name="23"
+
+print(int(sal))
+print(float(age))
+print(int(name))
+print(str(boool))
