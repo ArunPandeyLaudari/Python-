ae724f9108ba02d5bfd44a369c2054345ce2d698
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-02-15 20:20:57 +0400

    Arthimetic opertaor

---
+++ areaoftriangle.py
@@ -0,0 +1,8 @@
+# program to calculate the area of Triangle 
+
+base=float(input("Enter the base of Triange"))
+height=float(input("Enter the Height of Triangle"))
+
+area=1/2*(base*height)
+
+print(f"Area of triangle having base :{base} & height :{height} is : {area} cm²")
---
+++ arithmeticoperator.py
@@ -0,0 +1,21 @@
+a=2
+b=20
+
+# a+=10
+# a-=2
+# b*=a
+# b/=2
+# for power of 2 
+# b**=2
+# b%=2
+print(b)
+
+# without mathmodule dunction
+
+
+x=34.99
+
+print(round(x))
+print(max(1,2,4))
+print(min(3,4,5))
+
